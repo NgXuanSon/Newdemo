1fe569d1e6f4741e5107afcee778a074fd0cdcdd
XuanSon <89241809+NgXuanSon@users.noreply.github.com>
2021-11-11 22:14:50 +0700

    Baitap

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Netmo.Models;
+using Netmo.Data;
 
 namespace Netmo.Controllers
 {
--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Netmo.Data;
+using Netmo.Models;
+using System;
+
+
 
 namespace Netmo
 {
@@ -13,7 +14,24 @@ namespace Netmo
     {
         public static void Main(string[] args)
         {
+            var host = CreateHostBuilder(args).Build();
             CreateHostBuilder(args).Build().Run();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
